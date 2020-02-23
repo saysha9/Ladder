237c25aff656e31c3453454bf30fafa8eca1ec9f
saysha9 <srj@hawaii.edu>
2020-02-23 06:19:43 +0000

    changed parseFloat to parseInt

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const bodyParser = require ("body-parser");
 const app = express();
 //to get and use html file
 app.use(bodyParser.urlencoded({extended: true}));
-app.get ("/", function (req, res) {
+app.get ("/ladder", function (req, res) {
   res.sendFile(__dirname + "/ladder.html")
 });
 
-app.post("/", function (req,res){
+app.post("/ladder", function (req,res){
   var num1 = Number(req.body.n1);
   var num2 = Number(req.body.n2);
 
@@ -22,8 +22,8 @@ app.post("/", function (req,res){
 });
 
 app.post("/ladder", function(req,res){
-  var height = parseFloat(req.body.height);
-  var depth = parseFloat (req.body.depth);
+  var height = parseInt(req.body.height);
+  var depth = parseInt (req.body.depth);
 
   var ladder = Math.sqrt(Math.pow(height,2) + (Math.pow(depth,2)));
   res.send ( "The Ladder is " + ladder.toFixed(0) + " feet long");
